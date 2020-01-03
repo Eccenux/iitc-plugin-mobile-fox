7e322c3fdeca087e1041dbd8e50810bb20f6dabe
eccenux <eccenux+git@gmail.com>
2020-01-03 18:46:50 +0100

    gulp concat and monekyscript base

--- my-plugin/gulpfile.js
+++ my-plugin/gulpfile.js
@@ -1,7 +1,22 @@
 /* global exports */
+var ms = require('gulp-monkeyscript');
+var msProject = ms.createProject("monkeyscript.json");
 
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+/**
+ * Default gulp task.
+ * @param {Function} cb Some callback.
+ */
 function defaultTask(cb) {
-    // place code for your default task here
+     
+    gulp.src("src/**/*.js")
+        .pipe(concat("script.user.js"))
+        .pipe(msProject())
+        .pipe(gulp.dest("dist/"));
+    
+    
     cb();
 }
 
