35e0c869afb175901d93fb37d50c84e87ff15d77
eccenux <eccenux+git@gmail.com>
2020-01-04 01:41:03 +0100

    proper meta data

--- my-plugin/gulpfile.js
+++ my-plugin/gulpfile.js
@@ -5,6 +5,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 
 /**
+ * TODO: copy info from package to monkey script.
+ * add update/download URL from package JSON?
+ * make sure id.catory is supported.
+ */
+function packageToMonkey() {
+}
+
+/**
  * Default gulp task.
  * @param {Function} cb Some callback.
  */
@@ -12,7 +20,7 @@ function defaultTask(cb) {
      
     gulp.src("src/**/*.js")
         .pipe(concat("script.user.js"))
-        .pipe(msProject())
+        .pipe(msProject()) // append Tampermonkey header
         .pipe(gulp.dest("dist/"));
     
     
