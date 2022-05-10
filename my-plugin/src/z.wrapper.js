4277062116d7ca66fb8e1c796c630abc5c579c3d
Nux <eccenux+git@gmail.com>
2022-05-10 03:19:14 +0200

    wrapper

---
+++ my-plugin/src/z.wrapper.js
@@ -0,0 +1,18 @@
+﻿// PLUGIN END //////////////////////////////////////////////////////////
+
+// only small wrapper to allow debugging
+function wrapper(plugin_info) {
+	var setup =  window.plugin.mobileFoxUx.setup;
+
+setup.info = plugin_info; //add the script info data to the function as a property
+if(!window.bootPlugins) window.bootPlugins = [];
+window.bootPlugins.push(setup);
+// if IITC has already booted, immediately run the 'setup' function
+if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
+(document.body || document.head || document.documentElement).appendChild(script);
