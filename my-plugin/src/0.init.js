99e8011d7301292aa8156aa21ebf8123d747e868
Nux <eccenux+git@gmail.com>
2022-05-10 03:11:47 +0200

    split

---
+++ my-plugin/src/0.init.js
@@ -0,0 +1,14 @@
+﻿// original wrapper
+//function wrapper(plugin_info) {
+
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+// use own namespace for plugin
+window.plugin.mobileFoxUx = function() {};
+
+/**
+ * CSS
+ */
+// _css will be provided via `gulp` build.
+window.plugin.mobileFoxUx.CSS = _css;
