f25a83e0919936dafbf455717517743786cf0d38
Nux <eccenux+git@gmail.com>
2022-05-10 03:27:52 +0200

    Mission dialogs width: 95vw

--- my-plugin/src/class.MissionsOverwrite.js
+++ my-plugin/src/class.MissionsOverwrite.js
@@ -1,4 +1,9 @@
-﻿let missionsOverwrite = {
+﻿/**
+ * Missions plugin overwrites.
+ * 
+ * Move it to separate plugin?
+ */
+let missionsOverwrite = {
 	showMissionListDialog : function (missions) {
 		console.log('[missionsOverwrite]', 'showMissionListDialog');
 
@@ -31,4 +36,32 @@
 			this.onPaneChanged(pane);
 		});
 	},
+
+	showMissionDialog: function(mission) {
+		var me = this;
+		var markers = this.drawMission(mission);
+		var content = this.renderMission(mission);
+		var id = mission.guid.replace(/\./g, '_'); // dots irritate the dialog framework and are not allowed in HTML IDs
+		
+		dialog({
+			id: 'plugin-mission-details-' + id,
+			title: mission.title,
+			height: 'auto',
+			html: content,
+			width: '95vw',
+			closeCallback: function() {
+				me.removeMissionLayers(markers);
+			},
+			collapseCallback: this.collapseFix,
+			expandCallback: this.collapseFix,
+			focus: function() {
+				me.highlightMissionLayers(markers);
+			}
+		}).dialog('option', 'buttons', {
+			'Zoom to mission': function() {
+				me.zoomToMission(mission);
+			},
+			'Close': function() { $(this).dialog('close'); },
+		});
+	},	
 }
