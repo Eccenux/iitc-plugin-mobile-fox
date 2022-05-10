cd5022fc682d8eeb5d5e8df762285186245fa676
Nux <eccenux+git@gmail.com>
2022-05-10 03:33:29 +0200

    build

--- mobile-fox.user.js
+++ mobile-fox.user.js
@@ -218,75 +218,6 @@ window.plugin.mobileFoxUx.setup = function() {
 };
 
 /**
- * Missions plugin overwrites.
- * 
- * @todo Move this to separate plugin?
- */
-let missionsOverwrite = {
-	showMissionListDialog : function (missions) {
-		console.log('[missionsOverwrite]', 'showMissionListDialog');
-
-		let contentElement = this.renderMissionList(missions);
-		this.mobilePaneContainer.appendChild(contentElement);
-	},
-
-	onPaneChanged : function (pane) {
-		console.log('[missionsOverwrite]', 'onPaneChanged', this);
-
-		if(pane == 'plugin-missions') {
-			document.body.appendChild(this.mobilePane);
-			$(this.mobilePaneContainer).empty();
-			this.openTopMissions();
-		} else if(this.mobilePane.parentNode) {
-			this.mobilePane.parentNode.removeChild(this.mobilePane);
-		}
-	
-	},
-	
-	createMissionsPane : function () {
-		console.log('[missionsOverwrite]', 'createMissionsPane', this);
-
-		this.mobilePane = document.createElement('div');
-		this.mobilePane.className = 'plugin-mission-pane';
-	
-		this.mobilePaneContainer = document.createElement('div');
-		this.mobilePane.appendChild(this.mobilePaneContainer);
-		addHook('paneChanged', (pane) => {
-			this.onPaneChanged(pane);
-		});
-	},
-
-	showMissionDialog: function(mission) {
-		var me = this;
-		var markers = this.drawMission(mission);
-		var content = this.renderMission(mission);
-		var id = mission.guid.replace(/\./g, '_'); // dots irritate the dialog framework and are not allowed in HTML IDs
-		
-		dialog({
-			id: 'plugin-mission-details-' + id,
-			title: mission.title,
-			html: content,
-			height: Math.ceil(document.documentElement.clientHeight * 0.9),
-			width: '95vw',
-			closeCallback: function() {
-				me.removeMissionLayers(markers);
-			},
-			focus: function() {
-				me.highlightMissionLayers(markers);
-			}
-		}).dialog('option', 'buttons', {
-			'Zoom to mission': function() {
-				me.zoomToMission(mission);
-				// show map and collapse dialog
-				show('map');
-				$(this).parent().find('.ui-dialog-titlebar-button-collapse').click();
-			},
-			'Close': function() { $(this).dialog('close'); },
-		});
-	},
-}
-
-/**
  * Add pane/panel.
  * 
  * Similar to `android.addPane`, but instead of resource name you can add a class to the link.
@@ -494,9 +425,74 @@ window.plugin.mobileFoxUx.showCheckpointsDialog = function (checkpointTime, cycl
 };
 /**/
 
+/**
+ * Missions plugin overwrites.
+ * 
+ * @todo Move this to separate plugin?
+ */
+let missionsOverwrite = {
+	showMissionListDialog : function (missions) {
+		console.log('[missionsOverwrite]', 'showMissionListDialog');
 
-//var setup =  window.plugin.mobileFoxUx.setup;
+		let contentElement = this.renderMissionList(missions);
+		this.mobilePaneContainer.appendChild(contentElement);
+	},
 
+	onPaneChanged : function (pane) {
+		console.log('[missionsOverwrite]', 'onPaneChanged', this);
+
+		if(pane == 'plugin-missions') {
+			document.body.appendChild(this.mobilePane);
+			$(this.mobilePaneContainer).empty();
+			this.openTopMissions();
+		} else if(this.mobilePane.parentNode) {
+			this.mobilePane.parentNode.removeChild(this.mobilePane);
+		}
+	
+	},
+	
+	createMissionsPane : function () {
+		console.log('[missionsOverwrite]', 'createMissionsPane', this);
+
+		this.mobilePane = document.createElement('div');
+		this.mobilePane.className = 'plugin-mission-pane';
+	
+		this.mobilePaneContainer = document.createElement('div');
+		this.mobilePane.appendChild(this.mobilePaneContainer);
+		addHook('paneChanged', (pane) => {
+			this.onPaneChanged(pane);
+		});
+	},
+
+	showMissionDialog: function(mission) {
+		var me = this;
+		var markers = this.drawMission(mission);
+		var content = this.renderMission(mission);
+		var id = mission.guid.replace(/\./g, '_'); // dots irritate the dialog framework and are not allowed in HTML IDs
+		
+		dialog({
+			id: 'plugin-mission-details-' + id,
+			title: mission.title,
+			html: content,
+			height: Math.ceil(document.documentElement.clientHeight * 0.9),
+			width: '95vw',
+			closeCallback: function() {
+				me.removeMissionLayers(markers);
+			},
+			focus: function() {
+				me.highlightMissionLayers(markers);
+			}
+		}).dialog('option', 'buttons', {
+			'Zoom to mission': function() {
+				me.zoomToMission(mission);
+				// show map and collapse dialog
+				show('map');
+				$(this).parent().find('.ui-dialog-titlebar-button-collapse').click();
+			},
+			'Close': function() { $(this).dialog('close'); },
+		});
+	},	
+}
 /**
  * Helper class for detecting swipe events.
  * 
