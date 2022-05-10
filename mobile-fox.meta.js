972bd4aabe324d07a20264b1143b2e9941c4fbee
Nux <eccenux+git@gmail.com>
2022-05-10 04:09:19 +0200

    0.2.2: build refactor

--- mobile-fox.meta.js
+++ mobile-fox.meta.js
@@ -4,8 +4,8 @@
 // @id          iitc-plugin-mobile-fox@jonatkins
 // @category    Misc
 // @namespace   https://github.com/jonatkins/ingress-intel-total-conversion
-// @version     0.2.1
-// @description [0.2.1] Plugin focused on making IITC better for mobile phones. Mostly for users of Firefox for Android.
+// @version     0.2.2
+// @description [0.2.2] Plugin focused on making IITC better for mobile phones. Mostly for users of Firefox for Android.
 // @match       https://*.ingress.com/intel*
 // @match       http://*.ingress.com/intel*
 // @match       https://*.ingress.com/mission/*
